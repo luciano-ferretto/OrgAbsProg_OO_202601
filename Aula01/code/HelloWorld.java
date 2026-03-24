defb16c64679f2e8fb55ece9d1727db0b8a31bba
luciano.ferretto <luciano.ferretto@atitus.edu.br>
2026-03-23 21:51:03 -0300

    Aula01 - Códigos feitos em aula

---
+++ Aula01/code/HelloWorld.java
@@ -0,0 +1,8 @@
+void main() {
+    System.out.println("Hello World!");
+    System.out.println("Teste");
+
+    int var1 = 10;
+    int var2 = 4;
+    System.out.println(var1 / var2);
+}
---
+++ Aula01/code/Teste.py
@@ -0,0 +1,14 @@
+var1 = 10
+var2 = 4
+print(var1 / var2)
+
+
+print("Hello World!!!")
+
+x = "5"
+#y = x + 2
+
+def somar(a, b) :
+    c = a + b
+    return c
+somar(3,2)
