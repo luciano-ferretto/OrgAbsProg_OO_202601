4a13d01be78c750882982d166fb1ed54d40ce0e3
luciano.ferretto <luciano.ferretto@atitus.edu.br>
2026-03-24 19:17:52 -0300

    Aula02 - Material

--- Aula01/code/HelloWorld.java
+++ Aula01/code/HelloWorld.java
@@ -1,8 +1,4 @@
 void main() {
-    System.out.println("Hello World!");
-    System.out.println("Teste");
-
-    int var1 = 10;
-    int var2 = 4;
-    System.out.println(var1 / var2);
+    System.out.println("Hello World");
+    
 }
